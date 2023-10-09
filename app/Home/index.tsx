ba5ebc21c41168779e0dd16e52cc997d84f23a6e
Simba Wangu <simbawangulu@gmail.com>
2023-10-09 09:30:10 +0300

    initialize app

---
+++ app/Home/index.tsx
@@ -0,0 +1,22 @@
+import { Pressable, Text, View } from 'react-native'
+import React from 'react'
+import tw from 'twrnc'
+import { useRouter } from 'expo-router'
+
+const index = (): JSX.Element => {
+  const router = useRouter()
+
+  return (
+    <View style={tw`h-full w-full items-center justify-center`}>
+      <Text style={tw`text-blue-500 text-4xl text-center my-4`}>Home Page</Text>
+      <Pressable
+        style={tw`h-20 w-8/12 border-blue-500 border-2 rounded-xl items-center justify-center`}
+        onPress={() => router.push('/')}
+      >
+        <Text style={tw`text-blue-500 text-2xl text-center`}>Press Me</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default index
