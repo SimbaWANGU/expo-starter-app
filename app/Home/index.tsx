3256a16a195afe22b21f4fc9e250f9fd9785fae6
Simba Wangu <simbawangulu@gmail.com>
2023-10-09 09:32:15 +0300

    update existing pages

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -9,6 +9,7 @@ const index = (): JSX.Element => {
   return (
     <View style={tw`h-full w-full items-center justify-center`}>
       <Text style={tw`text-blue-500 text-4xl text-center my-4`}>Home Page</Text>
+      <Text style={tw`text-blue-500 text-xl text-center mb-2`}>Open up /app/Home/index.tsx to start working on your app!</Text>
       <Pressable
         style={tw`h-20 w-8/12 border-blue-500 border-2 rounded-xl items-center justify-center`}
         onPress={() => router.push('/')}
--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,7 @@ const index = (): JSX.Element => {
   return (
     <View style={tw`h-full w-full items-center justify-center`}>
       <Text style={tw`text-red-500 text-4xl text-center my-4`}>Index Page</Text>
+      <Text style={tw`text-red-500 text-xl text-center mb-2`}>Open up /app/index.tsx to start working on your app!</Text>
       <Pressable
         style={tw`h-20 w-8/12 border-red-500 border-2 rounded-xl items-center justify-center`}
         onPress={() => router.push('/Home')}  
