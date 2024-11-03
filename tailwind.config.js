865a56a7560522e69cecdbc88796eb00741ddc30
SimbaWANGU <simbawangulu@gmail.com>
2024-11-03 14:24:04 +0300

    app configuratuons

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D4ED8', // example custom primary color
+        secondary: '#9333EA', // example custom secondary color
+        accent: '#F59E0B', // example accent color
+        background: '#F3F4F6', // example background color
+        textPrimary: '#111827', // example primary text color
+        // Add more custom colors as needed
+      },
+    },
+  },
+  plugins: [],
+};
